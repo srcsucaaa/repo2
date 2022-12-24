622fdfb1b8a997b4fa622516f9b278085df410ac
srcsucaaa <91120202+srcsucaaa@users.noreply.github.com>
2022-12-24 22:20:02 +0800

    初始化项目

---
+++ src/main/java/com/itheima/pojo/Employee.java
@@ -0,0 +1,50 @@
+package com.itheima.pojo;
+//员工持久化类
+public class Employee {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String position;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", position='" + position + '\'' +
+                '}';
+    }
+}
